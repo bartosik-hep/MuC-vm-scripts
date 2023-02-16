dd8f7ba6c8e392a8f3b020c7ea473787d5d5855e
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-02-16 04:39:27 +0100

    Added CVMFS setup

--- cs8-cern.sh
+++ cs8-cern.sh
@@ -1,6 +1,10 @@
 # Commands to set up basic CERN environment on an OpenStack VM instance
 # Starting from a clean CentOS 8: https://gitlab.cern.ch/linuxsupport/koji-image-build/-/blob/07832690/cs8-cloud.ks
 
+############################################## Create user account
+export USER=nbartosi
+adduser $USER
+
 ############################################## Set up EOS client
 # following: https://cern.service-now.com/service-portal?id=kb_article&sys_id=d3faa3af4fc2d6404b4abc511310c785
 cat > /etc/yum.repos.d/eos7-stable.repo << THEND
@@ -37,3 +41,36 @@ scp ${USER}@lxplus:/etc/auto.eos         /etc/
 # Start the `autofs` service to mount EOS folders
 systemctl start autofs
 
+
+############################################## Set up CVMFS
+# following: https://cvmfs.readthedocs.io/en/stable/cpt-quickstart.html
+yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+yum install -y cvmfs
+
+cvmfs_config setup
+
+cat >> /etc/auto.master << THEND
+
+# Include /etc/auto.master.d/*.autofs
+# The included files must conform to the format of this file.
+#
++dir:/etc/auto.master.d
+#
+# Include central master map if it can be found using
+# nsswitch sources.
+#
+# Note that if there are entries for /net or /misc (as
+# above) in the included master map any keys that are the
+# same will not be seen as the first read key seen takes
+# precedence.
+#
++auto.master
+/cvmfs  program:/etc/auto.cvmfs
+THEND
+
+scp ${USER}@lxplus:/etc/cvmfs/default.local /etc/cvmfs/
+systemctl restart autofs
+
+# Check access to all repositories
+cvmfs_config wipecache
+cvmfs_config probe
